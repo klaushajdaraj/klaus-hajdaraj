46d5754977cb856a3b979611a664d129f70e205a
Klaus Hajdaraj <klaus.hajdaraj1@gmail.com>
2025-08-03 09:17:36 +0200

    Added another vercel component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="font-mono">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
